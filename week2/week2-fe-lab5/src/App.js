6d851282d83f7f0d48033fb6ad70a611b7c9ebd3
35846 <ilkka.lofman@hotmail.fi>
2024-10-30 19:12:16 +0200

    react-labs done

---
+++ week2/week2-fe-lab5/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/week2-fe-lab7/src/Greetings.js
@@ -0,0 +1,12 @@
+function Greetings(props) {
+    console.log(props); // Log the props object
+    
+    return (
+        <div className="greeting">
+            <h1>Welcome, {props.name}!</h1>
+            <p>{props.message}</p>
+        </div>
+    );
+}
+
+export default Greetings;
