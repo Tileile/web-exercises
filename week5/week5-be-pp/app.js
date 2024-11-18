2f2973ba9e9946ad3628bc95140c48891c74c9f8
35846 <ilkka.lofman@hotmail.fi>
2024-11-18 14:36:49 +0200

    Done monday week5-be-pp

---
+++ week5/week5-be-pp/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const tourRouter = require("./routes/tourRouter");
+const userRouter = require("./routes/userRouter");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
+const connectDB = require("./config/db"); 
+require('dotenv').config();
+
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+// Middleware to parse JSON
+app.use(express.json());
+
+connectDB();
+
+// Use the tourRouter for all "/tours" routes
+app.use("/api/tours", tourRouter);
+
+// Use the userRouter for all /users routes
+app.use("/api/users", userRouter);
+
+// Example route that throws an error
+app.get('/error', (req, res, next) => {
+  // Trigger an error
+  const error = new Error("Something went wrong!");
+  next(error);
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+
+const port = process.env.PORT || 4000;
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+ 
---
+++ week5/week5-be-pp/models/userModel.js
@@ -0,0 +1,49 @@
+/* {
+  "name": "Matti Seppänen",
+  "email": "matti@example.com",
+  "password": "M@45mtg$",
+  "phone_number": "+358401234567",
+  "gender": "Male",
+  "date_of_birth": "2000-01-15",
+  "membership_status": "Active"
+} */
+  const mongoose = require("mongoose");
+
+  const Schema = mongoose.Schema;
+ 
+  const userSchema = new Schema(
+  {
+      name: {
+      type: String,
+      required: true,
+      },
+      email: {
+      type: String,
+      required: true,
+      unique: true,
+      },
+      password: {
+      type: String,
+      required: true,
+      },
+      phone_number: {
+      type: String,
+      required: true,
+      },
+      gender: {
+      type: String,
+      required: true,
+      },
+      date_of_birth: {
+      type: Date,
+      required: true,
+      },
+      membership_status: {
+      type: String,
+      required: true,
+      },
+  },
+  { timestamps: true  }
+  );
+ 
+  module.exports = mongoose.model("User", userSchema);
